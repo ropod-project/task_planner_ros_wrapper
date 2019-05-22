a09f5cddf91ad593bd998aece9d2ebd638884bd1
Dharmin <dharmingeo@gmail.com>
2019-05-22 18:25:50 +0200

    Added ros related file for skeleton development

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+   packages=['task_planner_ros_wrapper'],
+   package_dir={'task_planner_ros_wrapper': 'ros/src/task_planner_ros_wrapper'}
+)
+
+setup(**d)
